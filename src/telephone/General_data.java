d66042a91afc57bdd4cf859df45d7e34b38fd835
Ulyana <uliana.malakhova@yandex.ru>
2023-01-13 17:04:21 +0700

    Исключения в General_data

--- src/telephone/General_data.java
+++ src/telephone/General_data.java
@@ -22,13 +22,26 @@ public class General_data {
         this.year = 0;
     }
     public void read(Scanner in){
+        int p = 0;
         System.out.println("Введем информацию об общих данных");
         System.out.print("Страна-производитель: ");
         this.country=in.next();
         System.out.print("Модель: ");
         this.model=in.next();
-        System.out.print("Год выпуска: ");
-        this.year=in.nextInt();
+        while (p == 0) {
+            p = 1;
+            System.out.print("Год выпуска: ");
+            this.year = in.nextInt();
+            try {
+                if (year < 1876)
+                    throw new Exception("Неверная информация, в этом году еще не было телефонов.");
+                if (year > 2023)
+                    throw new Exception("Неверная информация, этот год еще не наступил.");
+            } catch (Exception e) {
+                p = 0;
+                System.out.println(e + "Попробуйте ввести информацию заново");
+            }
+        }
     }
     public void display(){
         System.out.println("Общие данные:");
