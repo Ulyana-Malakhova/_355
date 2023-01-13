9e1d4b598bf5be60de4ae6abea431e1992cc7165
Ulyana <uliana.malakhova@yandex.ru>
2023-01-13 16:53:19 +0700

    Исключения в Telephone

--- src/telephone/Telephone.java
+++ src/telephone/Telephone.java
@@ -40,24 +40,76 @@ public class Telephone {
         screen.display();
         general_data.display();
     }
-    public void zaryad(){
-        System.out.print("Сколько стало процентов заряда на телефоне?");
-        int charge1=in.nextInt();
+    public void zaryad() {
+        int p = 0, charge1 = 0;
+        while (p == 0) {
+            p = 1;
+            System.out.print("Сколько стало процентов заряда на телефоне?");
+            charge1 = in.nextInt();
+            try {
+                if (current_state.getCharge() < 0)
+                    throw new Exception("Процент заряда меньше 0");
+                if (current_state.getCharge() > 100)
+                    throw new Exception("Процент заряда больше 100");
+            }
+            catch (Exception e) {
+                p = 0;
+                System.out.println(e+"Попробуйте ввести информацию заново");
+            }
+        }
         current_state.setCharge(charge1);
     }
     public void change(){
+        int p = 0, i = 0, j = 0, k = 0;
         int memory1 = current_state.getMemory();
         String card1 = system_t.getCard();
-        System.out.println("Изменения с объемом памяти связаны с:");
-        System.out.println("1.Изменением состояния карты памяти");
-        System.out.println("2.Изменением объема информации");
-        int i=in.nextInt();
+        while (p == 0) {
+            p = 1;
+            System.out.println("Изменения с объемом памяти связаны с:");
+            System.out.println("1.Изменением состояния карты памяти");
+            System.out.println("2.Изменением объема информации");
+            i = in.nextInt();
+            try {
+                if (i < 1)
+                    throw new Exception("Неправильные данные");
+                if (i > 2)
+                    throw new Exception("Неправильные данные");
+            } catch (Exception e) {
+                p = 0;
+                System.out.println(e + "Попробуйте выбрать действие заново");
+            }
+        }
         if (i == 1) {
-            System.out.println("1.Карту памяти ставят в телефон");
-            System.out.println("2.Карту памяти убирают из телефона");
-            int j=in.nextInt();
-            System.out.print("Какой объем памяти у карты?");
-            int k=in.nextInt();
+            p = 0;
+            while (p == 0) {
+                p = 1;
+                System.out.println("1.Карту памяти ставят в телефон");
+                System.out.println("2.Карту памяти убирают из телефона");
+                j = in.nextInt();
+                try {
+                    if (j < 1)
+                        throw new Exception("Неправильные данные");
+                    if (j > 2)
+                        throw new Exception("Неправильные данные");
+                } catch (Exception e) {
+                    p = 0;
+                    System.out.println("Попробуйте выбрать действие заново");
+                }
+            }
+            p = 0;
+            while (p == 0) {
+                p = 1;
+                System.out.print("Какой объем памяти у карты?");
+                k = in.nextInt();
+                try {
+                    if (k < 0)
+                        throw new Exception("Неправильные данные");
+                    ;
+                } catch (Exception e) {
+                    p = 0;
+                    System.out.println(e + "Попробуйте ввести информацию заново");
+                }
+            }
             if (j == 1) {
                 if (card1.equals("no")==true) {
                     memory1 += k;
@@ -78,11 +130,35 @@ public class Telephone {
             }
         }
         if (i == 2) {
-            System.out.println("1.В телефон добавляют информацию");
-            System.out.println("2.Из телефона удаляют информацию");
-            int j=in.nextInt();
-            System.out.print("Какой объем памяти добавили или удалили?");
-            int k=in.nextInt();
+            p = 0;
+            while (p == 0) {
+                p = 1;
+                System.out.println("1.В телефон добавляют информацию");
+                System.out.println("2.Из телефона удаляют информацию");
+                j = in.nextInt();
+                try {
+                    if (j < 1)
+                        throw new Exception("Неправильные данные");
+                    if (j > 2)
+                        throw new Exception("Неправильные данные");
+                } catch (Exception e) {
+                    p = 0;
+                    System.out.println("Попробуйте выбрать действие заново");
+                }
+            }
+            p = 0;
+            while (p == 0) {
+                p = 1;
+                System.out.print("Какой объем памяти добавили или удалили?");
+                k = in.nextInt();
+                try {
+                    if (k < 0)
+                        throw new Exception("Неправильные данные");
+                } catch (Exception e) {
+                    p = 0;
+                    System.out.println("Попробуйте ввести информацию заново");
+                }
+            }
             if (j == 1) {
                 memory1 -= k;
                 current_state.setMemory(memory1);
